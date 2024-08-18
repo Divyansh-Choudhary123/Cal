a1d05ffb670a33b5bd7531229cf03d3a3d23886b
divyansh <divyanshc494@gmail.com>
2024-08-18 14:34:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Calculater from './Components/Calculater';
+
+function App() {
+  return (
+    <div>
+      <Calculater/>
+    </div>
+  );
+}
+
+export default App;
